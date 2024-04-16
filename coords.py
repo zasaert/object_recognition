b19d4ac7b826f582a8aa846cf99683a558e6edd8
zasaert <155296671+zasaert@users.noreply.github.com>
2024-04-16 21:30:38 +0300

    ы

--- coords.py
+++ coords.py
@@ -5,6 +5,9 @@ import time
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
 
 # Установите путь к драйверу браузера (например, chromedriver.exe)
 
@@ -13,7 +16,7 @@ driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()
 # Откройте страницу Google Maps
 driver.get('https://www.mapcrunch.com/')
 
-count = 800
+count = 1000
 
 time.sleep(1)
 kys = driver.find_element(By.ID,'hidemainad')
@@ -46,29 +49,27 @@ options.click()
 
 search_button = driver.find_element(By.ID,'go-button')
 element = driver.find_element(By.TAG_NAME,'canvas')
+#zoom = driver.find_element(By.XPATH,'/html/body/div[2]/div[6]/div/div/div[13]/div[2]/div[3]/div/button[1]')
 
-compass= driver.find_element(By.CLASS_NAME,'gm-compass')
 
+search_button.click()  
+time.sleep(4) 
 i=0
+a=0
 while i <= count:
-    time.sleep(2)
-    location = element.location
-    size = element.size
-    screenshot = Image.open(BytesIO(driver.get_screenshot_as_png()))
-    element_screenshot = screenshot.crop((0, 80, 1200, 800))
-    screenshot_path = f'E:\dataset\street_view_screenshot_{i}.png'
-    element_screenshot.save(screenshot_path)
-    time.sleep(2)
-    search_button.click()
-    time.sleep(2)    
-    location = element.location
-    size = element.size
-    screenshot = Image.open(BytesIO(driver.get_screenshot_as_png()))
-    element_screenshot = screenshot.crop((0, 80, 1200, 800))
-    screenshot_path = f'E:\dataset\street_view_screenshot_{i+1}.png'
-    element_screenshot.save(screenshot_path)
-    time.sleep(2)
-    i+=1
+    temp = 0
+    time.sleep(0.5)
+    while temp <= 6:
+        location = element.location
+        size = element.size
+        screenshot = Image.open(BytesIO(driver.get_screenshot_as_png()))
+        element_screenshot = screenshot.crop((0, 80, 1200, 800))
+        screenshot_path = f'F:\dataset\street_view_screenshot_{i+temp}.png'
+        element_screenshot.save(screenshot_path)
+        time.sleep(1)
+        temp+=1
+    search_button.click()   
+    i+=6
 
 # закрываем браузер
 driver.quit()
